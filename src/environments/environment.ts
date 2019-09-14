981001597ec5a157965e0c8eec820ce43d1f627c
ionictemplate <53865250+ionictemplate-app@users.noreply.github.com>
2019-09-14 11:53:26 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyCHsR7HlpFl7-6JjKYsRfZ6ykSOi5P-f6E',
-    authDomain: 'fruit-b930e.firebaseapp.com',
-    databaseURL: 'https://fruit-b930e.firebaseio.com',
-    projectId: 'fruit-b930e',
-    storageBucket: 'fruit-b930e.appspot.com',
-    messagingSenderId: '785940413726',
-    appId: '1:785940413726:web:cd9ea2b1bd93e2e6'
-  }
+  production: false
 };
 
 /*
