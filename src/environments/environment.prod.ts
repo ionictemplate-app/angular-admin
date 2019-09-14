267e54b67b77882a8fc4f54dbfc6d238c434e27a
ionictemplate <53865250+ionictemplate-app@users.noreply.github.com>
2019-09-14 11:53:01 +0800

    Update environment.prod.ts
    remove google api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyCHsR7HlpFl7-6JjKYsRfZ6ykSOi5P-f6E',
-    authDomain: 'fruit-b930e.firebaseapp.com',
-    databaseURL: 'https://fruit-b930e.firebaseio.com',
-    projectId: 'fruit-b930e',
-    storageBucket: 'fruit-b930e.appspot.com',
-    messagingSenderId: '785940413726',
-    appId: '1:785940413726:web:cd9ea2b1bd93e2e6'
-  }
+  production: true
 };
